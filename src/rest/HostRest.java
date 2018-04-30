f7143f1d4fa8ee468c3feb0f3124f4d5f390621e
ilijagrbic <ilijagrb.ns@gmail.com>
2018-04-30 21:36:31 +0200

    Hope it works xD

---
+++ src/rest/HostRest.java
@@ -0,0 +1,48 @@
+package rest;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.bson.Document;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import dbClasses.HostDatabase;
+import model.Friendship;
+import model.Host;
+import rest.dto.ErrorDTO;
+
+@LocalBean
+@Path("/hosts")
+@Stateless
+public class HostRest {
+
+	@Inject
+	private HostDatabase hostDatabase;
+	
+	@POST
+    @Path("/registerHost")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response registerHost(Host newHost)
+    {
+        Document searchBy = new Document();
+        searchBy.append("address", newHost.getAddress());
+
+        Document found = (Document) hostDatabase.getCollection().find(searchBy).first();
+     
+        if(found!=null) {
+        	return Response.status(Response.Status.CONFLICT).entity(new ErrorDTO("Host with same ip adress already exists.")).build();
+        }else {
+        	return null;
+        }
+    }
+}
