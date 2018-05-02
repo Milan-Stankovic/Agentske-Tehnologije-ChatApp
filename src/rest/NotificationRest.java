6b3f94c02f6dd3a66646acd89a0241e98867b80f
ilijagrbic <ilijagrb.ns@gmail.com>
2018-05-02 20:19:22 +0200

    Some more.

--- src/rest/NotificationRest.java
+++ src/rest/NotificationRest.java
@@ -2,60 +2,96 @@ package rest;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.websocket.server.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.bson.Document;
+
+import dbClasses.UserDatabase;
+import model.Friendship;
 
 @LocalBean
 @Path("/notify")
 @Stateless
 public class NotificationRest {
 	
-	@GET
+	@Inject
+	private UserDatabase userDatabase;
+	
+	@POST
 	@Path("{username}/notifyFriendshipStart")
 	@Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-	private Response notifyWantedFriend(@PathParam("userName") String userName) {
-		return null;
+	private Response notifyWantedFriend(@PathParam("userName") String userName, Friendship addedFriendship) {
+		Document foundSender = (Document) userDatabase.getCollection().find(new Document("username", addedFriendship.getSender())).first();
+		Document foundReciver = (Document) userDatabase.getCollection().find(new Document("username", addedFriendship.getSender())).first();
+		
+		if(foundReciver==null||foundSender==null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}else {
+			//Notify via websocket
+			return null;
+		}
 	}
 	
-	@GET
+	@POST
 	@Path("{username}/notifyFriendshipEnd")
 	@Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-	private Response notifyFriendshipEnd(@PathParam("userName") String userName) {
-		return null;
+	private Response notifyFriendshipEnd(@PathParam("userName") String userName, Friendship deleted) {
+		Document foundSender = (Document) userDatabase.getCollection().find(new Document("username", deleted.getSender())).first();
+		Document foundReciver = (Document) userDatabase.getCollection().find(new Document("username", deleted.getSender())).first();
+		
+		if(foundReciver==null||foundSender==null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}else {
+			//Notify via websocket
+			return null;
+		}
 	}
 	
-	@GET
+	@POST
 	@Path("{username}/notifyFriendshipStateChange")
 	@Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-	private Response notifyFriendshiopChanged(@PathParam("userName") String userName) {
-		return null;
+	private Response notifyFriendshiopChanged(@PathParam("userName") String userName, Friendship updated) {
+		Document foundSender = (Document) userDatabase.getCollection().find(new Document("username", updated.getSender())).first();
+		Document foundReciver = (Document) userDatabase.getCollection().find(new Document("username", updated.getSender())).first();
+		
+		if(foundReciver==null||foundSender==null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}else {
+			//Notify via websocket
+			return null;
+		}
 	}
 	
-	@GET
+	@POST
 	@Path("{username}/notifyNewGroup")
 	@Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
 	private Response notifyNewGroup(@PathParam("userName") String userName) {
+		//notify via websocket
 		return null;
 	}
 	
-	@GET
+	@POST
 	@Path("{username}/notifyEndGroup")
 	@Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
 	private Response notifyEndGroup(@PathParam("userName") String userName) {
+		//notify via websocket
 		return null;
 	}
 	
-	@GET
+	@POST
 	@Path("{username}/notifyNewGroupMember")
 	@Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
@@ -63,7 +99,7 @@ public class NotificationRest {
 		return null;
 	}
 	
-	@GET
+	@POST
 	@Path("{username}/notifyRemovedUser")
 	@Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
