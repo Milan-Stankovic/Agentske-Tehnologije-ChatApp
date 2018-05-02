e5b6f91fe7bf28b31d9c491933b1be200ed7bd31
Milan-Stankovic <milans995@gmail.com>
2018-05-03 01:49:17 +0200

    Odradjen rest za login, logout, register i notifikacije za prijatelje

--- src/rest/UserRest.java
+++ src/rest/UserRest.java
@@ -12,15 +12,24 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.bson.Document;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 
 import com.google.gson.Gson;
+import com.mongodb.client.FindIterable;
 
 import dbClasses.UserDatabase;
+import model.Friendship;
+import model.FriendshipStatus;
 import model.Message;
+import model.NotificationDTO;
+import model.NotificationType;
 import model.User;
 
 @LocalBean
@@ -31,11 +40,14 @@ public class UserRest {
 	@Inject
 	private UserBean users;
 	
-	/*@Inject
-	private WebSocket ws;*/
+	@Inject
+	private WebSocket ws;
 	
 	@Inject 
 	private UserDatabase userDatabase;
+	
+	@Inject
+	private PushNotifications wsPushNotif;
 	
 
 	@GET
@@ -62,6 +74,7 @@ public class UserRest {
    	      if(!active) {
    	    	  
    	    	users.getActiveUsers().add(person);
+   	    	
    	      }
    	      returnMessage="ACTIVE";
    	      
@@ -73,6 +86,143 @@ public class UserRest {
 		
 	}
 	
+	
+	private void alertFriends(String userName, boolean remove) {
+		
+		Gson gson = new Gson();
+		
+	    Document search1 = new Document();
+  	     search1.append("sender", userName);
+  	     search1.append("status", FriendshipStatus.ACCEPTED);
+  	     
+  	     @SuppressWarnings("unchecked")
+  	     FindIterable<Document> docs = userDatabase.getCollection().find(search1);
+  	     for (Document doc : docs) {
+	      Friendship friend = gson.fromJson(doc.toJson(), Friendship.class);
+	      for (User u : users.getActiveUsers()) {
+	    	  if(u.getUsername().equals(friend.getReciever())) {
+	    		  NotificationDTO temp = new NotificationDTO();
+	    		  temp.setRecieverId(u.getUsername());
+	    		  temp.setUserId(userName);
+	    		  if(remove)
+	    			  temp.setType(NotificationType.LOGOUT);
+	    		  else
+	    			  temp.setType(NotificationType.LOGIN);
+	    		  wsPushNotif.pushNotification(temp);
+	    	  }
+			
+	      }
+  	    }
+  	     
+  	     
+  	     Document search2 = new Document();
+  	     search2.append("reciever", userName);
+  	     search2.append("status", FriendshipStatus.ACCEPTED);
+  	     
+  	    @SuppressWarnings("unchecked")
+ 	     FindIterable<Document> docs2 = userDatabase.getCollection().find(search2);
+ 	     for (Document doc : docs2) {
+	      Friendship friend = gson.fromJson(doc.toJson(), Friendship.class);
+	      for (User u : users.getActiveUsers()) {
+	    	  if(u.getUsername().equals(friend.getSender())) {
+	    		  NotificationDTO temp = new NotificationDTO();
+	    		  temp.setRecieverId(u.getUsername());
+	    		  temp.setUserId(userName);
+	    		  if(remove)
+	    			  temp.setType(NotificationType.LOGOUT);
+	    		  else
+	    			  temp.setType(NotificationType.LOGIN);
+	    		  wsPushNotif.pushNotification(temp);
+	    	  }
+			
+	      }
+ 	    }
+		
+	}
+	
+	
+	@POST
+    @Path("/login/{userName}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String login(@PathParam("userName") String userName, String password){
+		String returnStr="";
+	 	Document found = (Document) userDatabase.getCollection().find(new Document("username", userName));
+	 	if(found != null) {
+	   		  Gson gson = new Gson();
+	   	      User user = gson.fromJson(found.toJson(), User.class);  
+	   	      if(user.getUsername().equals(password)) {
+	   	    	  
+	   	    	ResteasyClient client = new ResteasyClientBuilder().build();
+	 			
+	 			ResteasyWebTarget target = client.target(
+	 					"http://" + users.getMasterIp() + ":8096/UserApp/rest/login");
+	 			Response response = target.request(MediaType.APPLICATION_JSON).post(Entity.entity(user, "application/vnd.com.demo.user-management.user+xml;charset=UTF-8;version=1"));
+	 			returnStr = response.readEntity(String.class);
+	 			
+	 			  boolean active= false;
+	 	   	      for (User user2 : users.getActiveUsers()) {
+	 					if(user2.getUsername().equals(user.getUsername())) {
+	 						user2.setHostIp(users.getCurrentIp()); // ako ga nisi postavio smoricu se
+	 						active=true;
+	 					}
+	 						
+	 				}
+	 	   	      if(!active) {
+	 	   	    	  
+	 	   	    	users.getActiveUsers().add(user);
+	 	   	      }
+	 	   	      alertFriends(userName, false);
+	 			
+	 			
+	   	      }else
+	   	    	  returnStr="WRONG PASSWORD";
+	   	      
+	 	}else
+	 		returnStr="WRONG USERNAME";
+
+		
+		return returnStr;
+	}
+	
+	
+		@POST
+	    @Path("/logout")
+	    @Consumes(MediaType.APPLICATION_JSON)
+	    @Produces(MediaType.TEXT_PLAIN)
+	    public String logout(String userName){
+			
+			String returnStr="";
+		 	Document found = (Document) userDatabase.getCollection().find(new Document("username", userName));
+		 	if(found != null) {
+		   		  Gson gson = new Gson();
+		   	      User user = gson.fromJson(found.toJson(), User.class);  
+		  
+		   	    	ResteasyClient client = new ResteasyClientBuilder().build();
+		 			
+		 			ResteasyWebTarget target = client.target(
+		 					"http://" + users.getMasterIp() + ":8096/UserApp/rest/logout");
+		 			Response response = target.request(MediaType.APPLICATION_JSON).post(Entity.entity(user, "application/vnd.com.demo.user-management.user+xml;charset=UTF-8;version=1"));
+		 			returnStr = response.readEntity(String.class);
+		 		
+		 	   	      for (User user2 : users.getActiveUsers()) {
+		 					if(user2.getUsername().equals(user.getUsername())) {
+		 						users.getActiveUsers().remove(user2);
+		 						break;
+		 					}
+		 						
+		 				}
+		 	   	   
+		 	   	      alertFriends(userName, true);
+		 		
+		   	      
+		 	}else
+		 		returnStr="NO USER";
+
+			
+			return returnStr;
+		 
+	 }
 	
 	
 	@DELETE
@@ -108,6 +258,32 @@ public class UserRest {
 		
 		
 	}
+	
+	
+	 	@POST
+	    @Path("/register")
+	    @Consumes(MediaType.APPLICATION_JSON)
+	    @Produces(MediaType.TEXT_PLAIN)
+	    public String register(User user) {
+	 		String returnStr ="";
+	 		boolean allOk= false;
+	 		if(!user.getUsername().equals("") && !user.getLastName().equals("") && !user.getName().equals("") && !user.getPassword().equals(""))
+	 			if(user.getUsername().length()>0 && user.getLastName().length()>0 && user.getName().length()>0 && user.getPassword().length()>0 && user.getPassword().length()>0)
+	 				allOk=true;
+	 		
+	 		if(allOk) {
+	 		ResteasyClient client = new ResteasyClientBuilder().build();
+ 			
+ 			ResteasyWebTarget target = client.target(
+ 					"http://" + users.getMasterIp() + ":8096/UserApp/rest/register");
+ 			Response response = target.request(MediaType.APPLICATION_JSON).post(Entity.entity(user, "application/vnd.com.demo.user-management.user+xml;charset=UTF-8;version=1"));
+ 			returnStr = response.readEntity(String.class);
+	 		}else
+	 			returnStr="BAD INPUT";
+	 		return returnStr;
+	 		
+	 	}
+	
 
 	
 	
@@ -116,12 +292,12 @@ public class UserRest {
 	@Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
 	private void getMessage(Message m) {
-		/*try {
+		try {
 			ws.forwardMessage(m);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}*/
+		}
 		
 	}
 
