f464f6414f266eb751572e30b27ba218a0d1801e
Milan-Stankovic <milans995@gmail.com>
2018-05-03 22:07:20 +0200

    Bugfix

--- src/rest/UserRest.java
+++ src/rest/UserRest.java
@@ -322,7 +322,7 @@ public class UserRest {
 		   	    	ResteasyClient client = new ResteasyClientBuilder().build();
 		 			
 		 			ResteasyWebTarget target = client.target(
-		 					"http://" + users.getMasterIp() + ":8096/UserApp/rest/login");
+		 					"http://" + users.getMasterIp() + ":8096/UserApp/rest/users/login");
 		 			Response response = target.request(MediaType.APPLICATION_JSON).post(Entity.entity(user,MediaType.APPLICATION_JSON));
 		 			returnStr = response.readEntity(String.class);
 	   	    	 
